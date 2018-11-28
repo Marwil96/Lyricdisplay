4947f7bb486cdd909c6884ca988d948211c246db
William_MAC <william_martinsson@hotmail.com>
2018-11-28 20:07:44 +0100

    Updates

---
+++ precache-manifest.64921e10617f81412c782845f6fa3e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Lyricdisplay/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49e3256ffde62dbf955f",
+    "url": "/Lyricdisplay/static/js/runtime~main.49e3256f.js"
+  },
+  {
+    "revision": "e995ba76236c62c288a6",
+    "url": "/Lyricdisplay/static/js/main.e995ba76.chunk.js"
+  },
+  {
+    "revision": "80fdf641cd7e066bec65",
+    "url": "/Lyricdisplay/static/js/1.80fdf641.chunk.js"
+  },
+  {
+    "revision": "e995ba76236c62c288a6",
+    "url": "/Lyricdisplay/static/css/main.6c0f2768.chunk.css"
+  },
+  {
+    "revision": "2ad8017e4d03dfefafd177999a2d73d8",
+    "url": "/Lyricdisplay/index.html"
+  }
+];
